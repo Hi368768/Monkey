bb2542e3a18cd2f44a57d1a755991e47e98f4545
konez2k <konez2k@gmail.com>
2018-03-18 01:19:01 +0100

    update last PoW block

--- src/main.h
+++ src/main.h
@@ -27,7 +27,7 @@ class CInv;
 class CRequestTracker;
 class CNode;
 
-static const int LAST_POW_BLOCK = 100000;
+static const int LAST_POW_BLOCK = 75000;
 
 static const unsigned int MAX_BLOCK_SIZE = 1000000;
 static const unsigned int MAX_BLOCK_SIZE_GEN = MAX_BLOCK_SIZE/2;
